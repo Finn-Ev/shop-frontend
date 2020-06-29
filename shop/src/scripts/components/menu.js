bc71c9d499b5e251d253fa684925c924e967ac2b
finn-ev <finn.evermann@leonex.de>
2020-06-29 16:53:49 +0200

    basic product layout and swiper

--- shop/src/scripts/components/menu.js
+++ shop/src/scripts/components/menu.js
@@ -1,55 +1,55 @@
-require(["jquery", "mmenu"], function ($, mmenu) {
-    jQuery(function ($) {
-        //Mmenu.configs.offCanvas.page.selector = "#my-page";
-        var mm = jQuery("#mobileMenu");
+// require(["jquery", "mmenu"], function ($, mmenu) {
+//     jQuery(function ($) {
+//         //Mmenu.configs.offCanvas.page.selector = "#my-page";
+//         var mm = jQuery("#mobileMenu");
 
-        mm.mmenu(
-            {
-                // options
-                slidingSubmenus: false,
-                extensions: [
-                    "pagedim-black",
-                    "position-left",
-                    "border-none",
-                    "position-front",
-                    "shadow-menu",
-                    "shadow-panels",
-                    //"fullscreen"
-                ],
-                iconPanels: false,
-                navbar: {
-                    title:
-                        "<img class='navbar-title' src='images/desktop/bugatti-logo.svg' />",
-                },
-                navbars: [
-                    {
-                        position: "top",
-                        content: "<i class='fas fa-times navbar-close'></i>",
-                    },
-                ],
-            },
-            {
-                // configuration
+//         mm.mmenu(
+//             {
+//                 // options
+//                 slidingSubmenus: false,
+//                 extensions: [
+//                     "pagedim-black",
+//                     "position-left",
+//                     "border-none",
+//                     "position-front",
+//                     "shadow-menu",
+//                     "shadow-panels",
+//                     //"fullscreen"
+//                 ],
+//                 iconPanels: false,
+//                 navbar: {
+//                     title:
+//                         "<img class='navbar-title' src='images/desktop/bugatti-logo.svg' />",
+//                 },
+//                 navbars: [
+//                     {
+//                         position: "top",
+//                         content: "<i class='fas fa-times navbar-close'></i>",
+//                     },
+//                 ],
+//             },
+//             {
+//                 // configuration
 
-                offCanvas: {
-                    pageSelector: "#js-mmenu-wrapper",
-                },
-                classNames: {
-                    fixedElements: {
-                        //fixed: "fix",
-                        //sticky: "sticky-top"
-                    },
-                },
-            }
-        );
-        mobileMenu.data = jQuery("#mobileMenu").data("mmenu");
+//                 offCanvas: {
+//                     pageSelector: "#js-mmenu-wrapper",
+//                 },
+//                 classNames: {
+//                     fixedElements: {
+//                         //fixed: "fix",
+//                         //sticky: "sticky-top"
+//                     },
+//                 },
+//             }
+//         );
+//         mobileMenu.data = jQuery("#mobileMenu").data("mmenu");
 
-        $(document).on("click", ".mobile-menu-toggle", function () {
-            mobileMenu.data.open();
-        });
+//         $(document).on("click", ".mobile-menu-toggle", function () {
+//             mobileMenu.data.open();
+//         });
 
-        $(document).on("click", ".navbar-close ", function () {
-            mobileMenu.data.close();
-        });
-    });
-});
+//         $(document).on("click", ".navbar-close ", function () {
+//             mobileMenu.data.close();
+//         });
+//     });
+// });
--- shop/src/scripts/components/swiper.js
+++ shop/src/scripts/components/swiper.js
@@ -3,14 +3,16 @@ require(["jquery", "swiper"], function ($, Swiper) {
         var swiper = new Swiper(".swiper-container", {
             slidesPerView: 2,
             breakpoints: {
-                // when window width is >= 320px
                 320: {
                     slidesPerView: 2,
+                    spaceBetween: 10,
+                },
+                568: {
+                    slidesPerView: 3,
                     spaceBetween: 20,
                 },
-                // when window width is >= 480px
                 768: {
-                    slidesPerView: 3,
+                    slidesPerView: 4,
                     spaceBetween: 30,
                 },
             },
